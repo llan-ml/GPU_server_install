455a0aa8ebbd48c327fc5a4c0b22bf8c095c77aa
Lin <ryan.linlan@gmail.com>
2017-07-22 20:59:29 +0800

    update

--- install_kubeadm.sh
+++ install_kubeadm.sh
@@ -2,14 +2,14 @@
 
 ssh -t root@localhost ' /bin/bash -i -c "
   set -e
-	snap install kubectl --classic
+  snap install kubectl --classic
   apt update && apt -y install apt-transport-https
   proxy_on apt
-	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-	cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-	deb http://apt.kubernetes.io/ kubernetes-xenial main
-	EOF
-	apt-get update
-	apt-get install -y kubelet kubeadm
-	proxy_off apt
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+  deb http://apt.kubernetes.io/ kubernetes-xenial main
+  EOF
+  apt-get update
+  apt-get install -y kubelet kubeadm
+  proxy_off apt
 "'
